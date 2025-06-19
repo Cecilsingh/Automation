4c136bcb6018e3096e832f908e81c6d0eeca236a
Cecil Singh <54638818+Cecilsingh@users.noreply.github.com>
2025-06-19 14:02:52 +1000

    Create main.tf

---
+++ Terraform/EC2/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "ceciltf" {
+  ami                    = "ami-0aff18ec83b712f05" # Ubuntu 22.04 LTS (us-west-2)
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-<omitted>"
+  vpc_security_group_ids = ["sg-<omitted>"]
+
+  tags = {
+    "Name"           = "Cecil TF"
+    "owner"       = "<omitted>"
+    "project"     = "<omitted>"
+    "environment" = "<omitted>"
+  }
+}
